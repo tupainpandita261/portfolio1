d94334504ab8d9e7e381e86f27361553546e3873
Tupain Pandita <143537414+tupainpandita261@users.noreply.github.com>
2025-10-01 08:51:52 +0530

    Add files via upload

---
+++ react/javascript.js
@@ -0,0 +1,38 @@
+// console.log("hello");
+
+// console.log("how are u");
+//  setTimeout ((name, age )=>{
+// console.log("my name is "+name+age);
+// },2000,"Ahal",19);
+
+// console.log("what is u name");
+
+// setTimeout(()=>{
+// console.log("my name is raju");
+// },3000);
+// function hello(name){
+// console.log("hello"+name);
+// }
+// setTimeout(hello,3000,"ahal");
+  ////*****Important point :callback is simply a function which is passed as a parameter in another function/////
+//**** */  or we can say in call back we  can pass one function into another function  as a parameter/** */////
+// // function api() {
+// //   return new Promise((resolve, reject) => {
+// //     setTimeout(() => {
+// //       console.log("weather data");
+// //       resolve(200);
+// //     }, 2000);
+// //   });
+// // }
+
+// // async function getWeatherData() {
+// //   await api(1); // 1st call → waits 2 sec
+// //   await api(2); // 2nd call → waits another 2 sec
+// // }
+//  let promise = new Promise((resolve ,reject)=>{
+//   setTimeout(() => {
+//     console.log("hello")
+//     resolve("done")
+//   }, 5000);
+//  })
+//  promise()
